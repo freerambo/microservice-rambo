7111525951914ad1357333f9633e8eec4234f015
Sasha <afridman@ntu.edu.sg>
2016-09-06 15:36:33 +0800

    DB : added exception handling and logginf to add_variable SP

--- Database/StoredProcedures/add_variable.sql
+++ Database/StoredProcedures/add_variable.sql
@@ -1,24 +1,47 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `add_variable`(IN device_id INT, IN name VARCHAR(45), IN description VARCHAR(255), IN unit_id TINYINT, IN updating_duration TINYINT)
 BEGIN
-INSERT INTO `smes_microgrid`.`variable`
-(
-`device_id`,
-`name`,
-`description`,
-`unit_id`,
-`updating_duration`
-)
-VALUES
-(
-device_id,
-name,
-description,
-unit_id,
-updating_duration
+-- General error handler for any SQL exception
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN	
+-- If some part of loading wasn't successful, continue with next steps but log the problem
+	CALL smes_microgrid.log_error('smes_microgrid.add_device');
+    ROLLBACK; -- NOTE: Rollback statement should come AFTER Get Diagnostics  (that is inside log_error sp)
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: An error occurred when ADDING NEW DEVICE';
+END;
 
--- Get and Set commands can be added only when variable is assigned to IN/OUR parameter list
--- set_command_id,
--- get_command_id
-);
+START TRANSACTION;	
 
+-- Log the start of excecution
+	CALL smes_microgrid.log_info('smes_microgrid.add_variable', CONCAT('Start Adding new VARIABLE to Device with DeviceID =', device_id, ' Values Are:  ', 'TODO'));
+-- End of Logging
+
+
+		INSERT INTO `smes_microgrid`.`variable`
+		(
+		`device_id`,
+		`name`,
+		`description`,
+		`unit_id`,
+		`updating_duration`
+		)
+		VALUES
+		(
+		device_id,
+		name,
+		description,
+		unit_id,
+		updating_duration
+
+		-- Get and Set commands can be added only when variable is assigned to IN/OUR parameter list
+		-- set_command_id,
+		-- get_command_id
+		);
+
+
+	-- Log the end of excecution
+		CALL smes_microgrid.log_info(@spName, CONCAT('Variable Added to Device ID=', device_id, ' New variable ID is: ', LAST_INSERT_ID()));
+	-- End of Logging
+
+	COMMIT;  
+    
 END
