47d6ee8ee2c5c1e5f565c3c57ac136cca890bd4c
Sasha <afridman@ntu.edu.sg>
2016-09-06 14:11:19 +0800

    DB : exception handling for add_device and update_device SP

--- Database/StoredProcedures/add_device.sql
+++ Database/StoredProcedures/add_device.sql
@@ -15,35 +15,53 @@ IN is_connected BIT
 )
 BEGIN
 
-INSERT INTO `smes_microgrid`.`device`
-(`device_type_id`,
-`name`,
-`description`,
-`microgrid_id`,
+-- General error handler for any SQL exception
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN	
+-- If some part of loading wasn't successful, continue with next steps but log the problem
+	CALL smes_microgrid.log_error('smes_microgrid.add_device');
+    ROLLBACK; -- NOTE: Rollback statement should come AFTER Get Diagnostics  (that is inside log_error sp)
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: An error occurred when ADDING NEW DEVICE';
+END;
 
-`vendor`,
-`model`,
-`location`,
-`ip_adress`,
-`port_number`,
-`bus_id`,
-`is_programmable`,
-`is_connected`)
-VALUES
-(device_type_id,
-name,
-description,
-microgrid_id,
+START TRANSACTION;	
 
-vendor,
-model,
-location,
-ip_adress,
-port_number,
-bus_id,
-is_programmable,
-is_connected
-);
+-- Log the start of excecution
+	CALL smes_microgrid.log_info('smes_microgrid.add_device', CONCAT('Start Adding new device, Values Are:  ', 'TODO'));
+-- End of Logging
 
+			INSERT INTO `smes_microgrid`.`device`
+						(`device_type_id`,
+						`name`,
+						`description`,
+						`microgrid_id`,
+						`vendor`,
+						`model`,
+						`location`,
+						`ip_adress`,
+						`port_number`,
+						`bus_id`,
+						`is_programmable`,
+						`is_connected`)
+			VALUES
+						(device_type_id,
+						name,
+						description,
+						microgrid_id,
+						vendor,
+						model,
+						location,
+						ip_adress,
+						port_number,
+						bus_id,
+						is_programmable,
+						is_connected
+						);
 
+	-- Log the end of excecution
+		CALL smes_microgrid.log_info('smes_microgrid.add_device', CONCAT('Device Added, New device ID is: ', LAST_INSERT_ID()));
+	-- End of Logging
+
+	COMMIT;  
+    
 END
--- Database/StoredProcedures/update_device.sql
+++ Database/StoredProcedures/update_device.sql
@@ -15,44 +15,45 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `update_device`(  IN id INT,
 BEGIN
 
 -- General error handler for any SQL exception
-DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
 BEGIN	
 -- If some part of loading wasn't successful, continue with next steps but log the problem
-	CALL smes_normalized.log_error('smes_microgrid.update_device');
+	CALL smes_microgrid.log_error('smes_microgrid.update_device');
     ROLLBACK; -- NOTE: Rollback statement should come AFTER Get Diagnostics  (that is inside log_error sp)
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: An error occurred when UPDATING A DEVICE';
 END;
 
 START TRANSACTION;	
 
 -- Log the start of excecution
-	CALL smes_normalized.log_info('smes_microgrid.update_device', CONCAT('Start device update ,ID= ', id));
+	CALL smes_microgrid.log_info('smes_microgrid.update_device', CONCAT('Start device update ,ID= ', id));
 -- End of Logging
 
--- Note - COALESCE takes first non-null value. it means, using it we can't set value to NULL (that is OK for now as we can set empty string for all the string columns)
-UPDATE smes_microgrid.device as D
-SET 
-`device_type_id` = COALESCE(device_type_id, D.device_type_id) ,
-`name` = COALESCE(name, D.name) ,
-`description` = COALESCE( description, D.description) ,
-`microgrid_id` = COALESCE( microgrid_id , D.microgrid_id),
+	-- Note - COALESCE takes first non-null value. it means, using it we can't set value to NULL (that is OK for now as we can set empty string for all the string columns)
+	UPDATE smes_microgrid.device as D
+	SET 
+	`device_type_id` = COALESCE(device_type_id, D.device_type_id) ,
+	`name` = COALESCE(name, D.name) ,
+	`description` = COALESCE( description, D.description) ,
+	`microgrid_id` = COALESCE( microgrid_id , D.microgrid_id),
 
-`vendor` = COALESCE( vendor, D.vendor),
-`model` = COALESCE( model, D.model),
-`location` = COALESCE(location , D.location),
-`ip_adress` = COALESCE(ip_adress , D.ip_adress),
-`port_number` = COALESCE( port_number, D.port_number),
-`bus_id` = COALESCE(bus_id , D.bus_id),
-`is_programmable` = COALESCE( is_programmable, D.is_programmable),
-`is_connected` = COALESCE( is_connected, D.is_connected) 
-WHERE D.id = id;
+	`vendor` = COALESCE( vendor, D.vendor),
+	`model` = COALESCE( model, D.model),
+	`location` = COALESCE(location , D.location),
+	`ip_adress` = COALESCE(ip_adress , D.ip_adress),
+	`port_number` = COALESCE( port_number, D.port_number),
+	`bus_id` = COALESCE(bus_id , D.bus_id),
+	`is_programmable` = COALESCE( is_programmable, D.is_programmable),
+	`is_connected` = COALESCE( is_connected, D.is_connected) 
+	WHERE D.id = id;
 
 
 
--- Log the end of excecution
-	CALL smes_microgrid.log_info('smes_microgrid.update_device', 'Device Updated, New values are: TODO');
--- End of Logging
+	-- Log the end of excecution
+		CALL smes_microgrid.log_info('smes_microgrid.update_device', 'Device Updated, New values are: TODO');
+	-- End of Logging
 
-COMMIT;  
+	COMMIT;  
 
 
 END
