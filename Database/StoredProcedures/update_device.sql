7a912674f8632d350bdee99bc2ce297c802cb805
Sasha <afridman@ntu.edu.sg>
2016-09-06 13:27:02 +0800

    DB : Added COALESCE to update_device to make it easy to skipp parameters that are not changed

--- Database/StoredProcedures/update_device.sql
+++ Database/StoredProcedures/update_device.sql
@@ -13,22 +13,25 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `update_device`(  IN id INT,
 									IN is_programmable BIT,
 									IN is_connected BIT)
 BEGIN
-
-UPDATE smes_microgrid.device 
+-- Note - COALESCE takes first non-null value. it means, using it we can't set value to NULL (that is OK for now as we can set empty string for all the string columns)
+UPDATE smes_microgrid.device as D
 SET 
-`device_type_id` = device_type_id,
-`name` = name,
-`description` = description,
-`microgrid_id` = microgrid_id,
+`device_type_id` = COALESCE(device_type_id, D.device_type_id) ,
+`name` = COALESCE(name, D.name) ,
+`description` = COALESCE( description, D.description) ,
+`microgrid_id` = COALESCE( microgrid_id , D.microgrid_id),
 
-`vendor` = vendor,
-`model` = model,
-`location` = location,
-`ip_adress` = ip_adress,
-`port_number` = port_number,
-`bus_id` = bus_id,
-`is_programmable` = is_programmable,
-`is_connected` = is_connected
-WHERE `id` = id;
+`vendor` = COALESCE( vendor, D.vendor),
+`model` = COALESCE( model, D.model),
+`location` = COALESCE(location , D.location),
+`ip_adress` = COALESCE(ip_adress , D.ip_adress),
+`port_number` = COALESCE( port_number, D.port_number),
+`bus_id` = COALESCE(bus_id , D.bus_id),
+`is_programmable` = COALESCE( is_programmable, D.is_programmable),
+`is_connected` = COALESCE( is_connected, D.is_connected) 
+WHERE D.id = id;
+
+
+
 
 END
