2951122db04f9ac0e62874a969b5a5663874683a
Sasha <afridman@ntu.edu.sg>
2016-09-13 17:00:06 +0800

    141 : API : fixed path for AddDevice method

--- Services/MicrogridApi/src/main/java/com/erian/microgrid/api/MicrogridApi/resources/DeviceResource.java
+++ Services/MicrogridApi/src/main/java/com/erian/microgrid/api/MicrogridApi/resources/DeviceResource.java
@@ -2,16 +2,16 @@ package com.erian.microgrid.api.MicrogridApi.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
-import com.erian.microgrid.api.MicrogridApi.service.DeviceHelper;
 import com.erian.microgrid.api.MicrogridApi.model.Device;
+import com.erian.microgrid.api.MicrogridApi.service.DeviceHelper;
 
 @Path("/devices")
 
@@ -32,9 +32,29 @@ public class DeviceResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	// @Path("/devices")  // POST action + "devices" is a recommended URI to "create new device" resource
+	 // POST action + "devices/" is a recommended URI to "create new device" resource
+	// URI to test : http://localhost:8080/MicrogridApi/devices?{"ID"=2,"IPAdress":"127.34.5646.12","busID":2,"classID":2,"className":"Source","description":"test Update Load 2 top Load 2device","isConnected":1,"isProgrammable":1,"location":"somewhere 2","microgridID":1,"microgridName":"Lab Level 5 Microgrid","model":"Model S2","name":"Load 2","portNumber":"3030upd2","typeID":2,"typeName":"DC source","vendor":"Some Vendor"} 
 	public Device addDevice(Device newDevice){
-		   return DeviceHelper.AddNewDevice(newDevice);
-		}
+		return DeviceHelper.AddNewDevice(newDevice);
+	}	
+	
+	/*
+	private Device GetFakeDevice()
+	{
+		Device newDevice1 = new Device();
+		newDevice1.setBusID(1);
+		newDevice1.setMicrogridID(1);
+		newDevice1.setName("API call add 3");
+		newDevice1.setTypeID(1);
+		newDevice1.setVendor("vendor3");
+		newDevice1.setModel("Model3");
+		newDevice1.setLocation("location3");
+		newDevice1.setIPAdress("some IP 3");
+		newDevice1.setPortNumber("2002");
+		newDevice1.setIsProgrammable(1);
+		newDevice1.setIsConnected(0);
+		return newDevice1;
+	}
+	*/
 }
 
