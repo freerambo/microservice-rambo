19f69293fa529d3f8b7e50e148bf2c8f344600d2
Sasha <afridman@ntu.edu.sg>
2016-09-13 15:10:34 +0800

    112 : DB : get variables SP - naming changed

--- Database/StoredProcedures/get_variables.sql
+++ Database/StoredProcedures/get_variables.sql
@@ -1,14 +1,14 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_variables`(IN device_id INT)
 BEGIN
 
-SELECT V.`id`,
-    V.`device_id`,
-    V.`name`,
-    V.`description`,
-    V.`unit_id`,
-    V.`updating_duration`,
-    V.`set_command_id`,
-    V.`get_command_id`
+SELECT V.`id` as ID,
+    V.`device_id` as DeviceID,
+    V.`name` as Name,
+    V.`description` as Description,
+    V.`unit_id` as UnitID,
+    V.`updating_duration` as UpdatingDuration,
+    V.`set_command_id` as SetCommandID,
+    V.`get_command_id` as GetCommandID
 FROM `smes_microgrid`.variable AS V
 LEFT JOIN `smes_microgrid`.variable_unit AS U ON U.id = V.unit_id
 WHERE V.device_id = device_id 
