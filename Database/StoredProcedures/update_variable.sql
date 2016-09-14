666a47fd5387290120d03a20b04ba367bd12820a
Sasha <afridman@ntu.edu.sg>
2016-09-14 11:21:05 +0800

    122 - DB - Changed update_variable SP to return the recently updated row

--- Database/StoredProcedures/update_variable.sql
+++ Database/StoredProcedures/update_variable.sql
@@ -37,4 +37,17 @@ WHERE `id` = variable_id;
 	-- End of Logging
 
 	COMMIT;  
+    
+SELECT V.`id` as ID,
+    V.`device_id` as DeviceID,
+    V.`name` as Name,
+    V.`description` as Description,
+    V.`unit_id` as UnitID,
+    V.`updating_duration` as UpdatingDuration,
+    V.`set_command_id` as SetCommandID,
+    V.`get_command_id` as GetCommandID
+FROM `smes_microgrid`.variable AS V
+LEFT JOIN `smes_microgrid`.variable_unit AS U ON U.id = V.unit_id
+WHERE V.id = variable_id; 
+    
 END
