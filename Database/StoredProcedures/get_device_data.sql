a57e8f6f581b88532ab5aff994685f9966f5b945
Sasha <afridman@ntu.edu.sg>
2016-09-20 13:57:34 +0800

    212 : DB : SP get_device_data returns a dataset with dynamically created columns, based on Variables registered for device

--- Database/StoredProcedures/get_device_data.sql
+++ Database/StoredProcedures/get_device_data.sql
@@ -1,16 +1,37 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_device_data`(IN id INT)
 BEGIN
 
--- TODO - make columns list dynamioc. Now 3 hardcoded volumns/variables to return some data for  API and GUI
-SELECT
-    VV.timestamp,
-    coalesce(sum(case when V.id = "9" then VV.value end), 0) as Power,
-    coalesce(sum(case when V.id= "10" then VV.value end), 0) as Voltage,
-    coalesce(sum(case when V.id = "11" then VV.value end), 0) as Current
-FROM smes_microgrid.variable_value as VV
-LEFT JOIN smes_microgrid.variable as V ON V.id= VV.variable_id
-WHERE V.device_id = 22
-GROUP BY VV.`timestamp`;
+-- General error handler for any SQL exception
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN	
+-- If some part of loading wasn't successful, continue with next steps but log the problem
+	CALL smes_microgrid.log_error('smes_microgrid.get_device_data');
+    ROLLBACK; -- NOTE: Rollback statement should come AFTER Get Diagnostics  (that is inside log_error sp)
+    SET @msg = CONCAT('ERROR: An error occurred when getting DEVICE DATA, Device id = ', id);
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @msg ;
+END;
 
+SET @sql = NULL;
+SELECT
+  GROUP_CONCAT(DISTINCT
+    CONCAT(
+      'coalesce(SUM(case when V.id = ''',
+      id,
+      ''' then VV.value end),'''') AS ',
+      name
+    )
+  ) INTO @sql
+FROM smes_microgrid.variable
+where device_id = id;
+
+SET @sql = CONCAT(  'SELECT VV.timestamp, ', @sql, 
+					' FROM smes_microgrid.variable_value as VV
+					LEFT JOIN smes_microgrid.variable as V ON V.id= VV.variable_id
+					GROUP BY VV.timestamp;');
+
+-- SELECT @sql;
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
 END
