f7bbcaa187b15c6692e573b5f66114694bf6e894
zhuyb <zhuyb@ERIANNB119.staff.main.ntu.edu.sg>
2016-09-20 21:02:28 +0800

    GUI: opt Device General info list/edit/add/delete

--- webapp/src/main/webapp/js/init.js
+++ webapp/src/main/webapp/js/init.js
@@ -22,15 +22,6 @@
                 .when('/login',
                         {templateUrl: 'partials/login.html',
                             publicAccess: true})
-               /* .when('/posts',
-                        {controller: 'PostsController',
-                            templateUrl: 'partials/posts/home.html'})
-                .when('/posts/new',
-                        {controller: 'NewPostController',
-                            templateUrl: 'partials/posts/new.html'})
-                .when('/posts/:id',
-                        {controller: 'DetailsController',
-                            templateUrl: 'partials/posts/details.html'})*/
                 .when('/devices',
                         {controller: 'DevicesController',
                             templateUrl: 'partials/devices/list.html',
@@ -41,7 +32,7 @@
                             publicAccess: true}) 
                  .when('/devices/update/:id',
                         {controller: 'UpdateDeviceController',
-                        templateUrl: 'partials/devices/edit.html',
+                        templateUrl: 'partials/devices/new.html',
                         publicAccess: true})            
                  .when('/devices/:id',
                         {controller: 'DeviceDetailsController',
@@ -54,11 +45,79 @@
         //configure $http to catch message responses and show them
 
 //        httpHeaders = $httpProvider.defaults.headers;
-        $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+//        $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
     });
 
 
+    as.run(function ($rootScope, $http, $route, $location, base64) {
+        //make current message accessible to root scope and therefore all scopes
+        $rootScope.message = function () {
+            return message;
+        };
 
+        /**
+         * Holds all the requests which failed due to 401 response.
+         */
+        $rootScope.requests401 = [];
+
+        $rootScope.$on('event:loginRequired', function () {
+            //$('#login').modal('show');
+            $location.path('/login');
+        });
+
+        /**
+         * On 'event:loginConfirmed', resend all the 401 requests.
+         */
+        $rootScope.$on('event:loginConfirmed', function () {
+            var i,
+                    requests = $rootScope.requests401,
+                    retry = function (req) {
+                        $http(req.config).then(function (response) {
+                            req.deferred.resolve(response);
+                        });
+                    };
+
+            for (i = 0; i < requests.length; i += 1) {
+                retry(requests[i]);
+            }
+
+            $rootScope.requests401 = [];
+            $location.path('/devices');
+        });
+
+        /**
+         * On 'event:loginRequest' send credentials to the server.
+         */
+        $rootScope.$on('event:loginRequest', function (event, username, password) {
+            httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
+            console.log('httpHeaders.common[\'Authorization\']@' + httpHeaders.common['Authorization'] + ':::' + username + ':' + password);
+            $http.get('api/me')
+                    .success(function (data) {
+                        $rootScope.authenticated = true;
+                        $rootScope.name = data.username;
+                        $rootScope.$broadcast('event:loginConfirmed');
+                    })
+                    .error(function (data) {
+                        console.log('login failed...@' + data);
+                    });
+        });
+
+        /**
+         * On 'logoutRequest' invoke logout on the server and broadcast 'event:loginRequired'.
+         */
+     
+
+        //$rootScope.$on('$viewContentChange', funtion());
+        //check the networking connection.
+
+        $http.get('api/ping')
+                .success(function (data) {
+                    console.log("ping result@"+data);
+                })
+                .error(function (data) {
+                     $rootScope.message={text:'Network connection eror!', type:'danger', show:true};
+                });
+    });
 
 }());
