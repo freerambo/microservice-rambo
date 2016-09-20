303669f5016973bc4311ae570e8b20fae6682a9a
Sasha <afridman@ntu.edu.sg>
2016-09-20 15:02:39 +0800

    145 : DB : stored procedude get_read_command(vaiableID), returns  formatString of the communication command and IP adress (and some other info) to get Variable value NOTE : for this sprint firmat string contains full command text. Later we apply some logic and parsing on it

---
+++ Database/StoredProcedures/get_read_command.sql
@@ -0,0 +1,16 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_read_command`(IN variable_id INT)
+BEGIN
+
+SELECT  V.id as variableId,
+		V.name as variableName,
+		C.format_string as commandFormatString,
+        C.id as commandId,
+        D.id as deviceId,
+        D.ip_adress as IPAdress,
+        D.port_number as portNumber
+FROM variable as V 
+INNER JOIN command as C ON C.id = V.get_command_id
+INNER JOIN device as D ON D.id = V.device_id
+WHERE V.id = variable_id;
+
+END
