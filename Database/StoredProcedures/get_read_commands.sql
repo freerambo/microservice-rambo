ca0d2b01c298f8f4d40b65f44d699a35035f2e93
Sasha <afridman@ntu.edu.sg>
2016-09-22 09:49:08 +0800

    177 : DB : Change get_read_commands for DataCollection service to support 1 device - 1 command reading

--- Database/StoredProcedures/get_read_commands.sql
+++ Database/StoredProcedures/get_read_commands.sql
@@ -1,6 +1,22 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_read_commands`()
 BEGIN
 
+SELECT 
+		C.format_string as commandFormatString,  -- for first version we assume there will be 1 command per device, but later many commands
+        C.id as commandId, 
+        C.name as commandName,
+        D.id as deviceId,
+        D.ip_adress as IPAdress,
+        D.port_number as portNumber,
+		GROUP_CONCAT(V.id) as variableIds,  -- for first version we assume all varables of the device expected to be returned by the read command
+		GROUP_CONCAT(V.name) as variableNames
+FROM device as D  
+INNER JOIN command as C ON C.device_id = D.id
+INNER JOIN variable as V ON D.id = V.device_id
+GROUP BY C.id  -- or b y D.id assuming that 1 device has 1 command only
+ORDER BY D.id;
+
+/* Put it back later when we resolve device concurrency problem
 SELECT  V.id as variableId,
 		V.name as variableName,
 		C.format_string as commandFormatString,
@@ -12,5 +28,7 @@ FROM variable as V
 INNER JOIN command as C ON C.id = V.get_command_id
 INNER JOIN device as D ON D.id = V.device_id
 ORDER BY D.id;
+*/
+
 
 END
