72050660a4034714f7500919a49ac14fe7c3bc4f
Sasha <afridman@ntu.edu.sg>
2016-09-23 16:12:03 +0800

    0 : DB : Latest Migration Script  and Data to demonstrate

--- Database/DataScripts/PopulateTestDataForDCLoad.sql
+++ Database/DataScripts/PopulateTestDataForDCLoad.sql
@@ -1,32 +1,33 @@
 SELECT * FROM smes_microgrid.device;
+select * from smes_microgrid.variable;
  
 START TRANSACTION;	
 
-CALL `smes_microgrid`.`add_device`(4, 'chroma_63211_dc_load', 'DC LOAD chroma_63211_dc_load', 1, 'chroma', 'chroma_63211', 'Lab at level 5', '172.21.76.125', '3030', 2, 1, 1);
+-- CALL `smes_microgrid`.`add_device`(4, 'chroma_63211_dc_load', 'DC LOAD chroma_63211_dc_load', 1, 'chroma', 'chroma_63211', 'Lab at level 5', '172.21.76.125', '3030', 2, 1, 1);
 
 -- 22 is ID of new Device, change accordingly
-CALL `smes_microgrid`.`add_variable`(22, 'Voltage', 'Voltage', 1, '5');
-CALL `smes_microgrid`.`add_variable`(22, 'Current', 'Current', 1, '5');
-CALL `smes_microgrid`.`add_variable`(22, 'Power', 'Power', 3, '5');
+-- CALL `smes_microgrid`.`add_variable`(22, 'Voltage', 'Voltage', 1, '5');
+-- CALL `smes_microgrid`.`add_variable`(22, 'Current', 'Current', 1, '5');
+-- CALL `smes_microgrid`.`add_variable`(22, 'Power', 'Power', 3, '5');
 
 -- select * from variable where device_id=22;
 
-delete from `smes_microgrid`.`variable_value` where variable_id=9;
-delete from `smes_microgrid`.`variable_value` where variable_id=10;
-delete from `smes_microgrid`.`variable_value` where variable_id=11;
+delete from `smes_microgrid`.`variable_value` where variable_id=21;
+delete from `smes_microgrid`.`variable_value` where variable_id=22;
+delete from `smes_microgrid`.`variable_value` where variable_id=23;
 -- delete from `smes_microgrid`.`variable_value`
 INSERT INTO `smes_microgrid`.`variable_value`
 											(`timestamp`,
 											`variable_id`,
 											`value`)
-SELECT system_timestamp, 9 , voltage
+SELECT system_timestamp, 21 , voltage
 FROM smartgrid.chroma_63211_dc_load;
 
 INSERT INTO `smes_microgrid`.`variable_value`
 											(`timestamp`,
 											`variable_id`,
 											`value`)
-SELECT system_timestamp, 10 , smartgrid.chroma_63211_dc_load.current
+SELECT system_timestamp, 22 , smartgrid.chroma_63211_dc_load.current
 FROM smartgrid.chroma_63211_dc_load;
 
 
@@ -34,7 +35,7 @@ INSERT INTO `smes_microgrid`.`variable_value`
 											(`timestamp`,
 											`variable_id`,
 											`value`)
-SELECT system_timestamp, 11 , power
+SELECT system_timestamp, 23 , power
 FROM smartgrid.chroma_63211_dc_load;
 
 COMMIT;
---
+++ Database/DataScripts/PopulateTestDataForDCLoad_1.sql
@@ -0,0 +1,3 @@
+CALL `smes_microgrid`.`add_variable`(26, 'Voltage', 'Voltage of the device',  3, 10);
+CALL `smes_microgrid`.`add_variable`(26, 'Current', 'Current of the device',  3, 10);
+CALL `smes_microgrid`.`add_variable`(26, 'Power', 'Power',  3, 10);
