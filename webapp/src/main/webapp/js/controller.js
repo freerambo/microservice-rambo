c85730fe8376c11ead759640c5e55f9f9184330d
zhuyb <zhuyb@ERIANNB119.staff.main.ntu.edu.sg>
2016-09-23 15:05:37 +0800

    GUI: integration with monitors

--- webapp/src/main/webapp/js/controller.js
+++ webapp/src/main/webapp/js/controller.js
@@ -157,11 +157,18 @@
                  $scope.variables = data;	                        
                 
             });
-        	$http.get(actionUrl+"/commands")
+/*        	$http.get(actionUrl+"/commands")
             .success(function (data) {
            	 
             	 console.log("Console commands " + data.id);
                  $scope.commands = data;	                        
+                
+            });*/
+        	
+        	$http.get(staticUrl+"/units")
+            .success(function (data) {
+           	 
+                 $scope.units = data;	                        
                 
             });
         	
@@ -176,10 +183,10 @@
 // save the variable
         
         $scope.saveVariable = function (id) {
-        	 $http.post(actionUrl + "variables", $scope.newVariable).success(function (data) {
-                 $location.path('/devices/'+id);
+        	$scope.newVariable.deviceID = id;
+        	 $http.post(actionUrl + "/variables", $scope.newVariable).success(function (data) {
         		 // push the new variables to the list
-//                 $scope.variables.push($scope.newVariable);
+                 $scope.variables.push(data);
              });
         	 
         };
@@ -224,6 +231,92 @@
         };
         
     });
+    
+    
+    
+    as.controller('devicedetailCtrl', function($scope, $http) {
+    	 $scope.itemsPerPage = 50;
+    	 $scope.currentPage = 0;
+    	 $scope.entities = [];
+    	 
+    	 $scope.range = function() {
+    		    var rangeSize = 4;
+    		    var ps = [];
+    		    var start;
+
+    		    start = $scope.currentPage;
+    		    if ( start > $scope.pageCount()-rangeSize ) {
+    		      start = $scope.pageCount()-rangeSize+1;
+    		    }
+
+    		    for (var i=start; i<start+rangeSize; i++) {
+    		      ps.push(i);
+    		    }
+    		    return ps;
+    		  };
+
+    		  $scope.prevPage = function() {
+    		    if ($scope.currentPage > 0) {
+    		      $scope.currentPage--;
+    		    }
+    		  };
+
+    		  $scope.DisablePrevPage = function() {
+    		    return $scope.currentPage === 0 ? "disabled" : "";
+    		  };
+
+    		  $scope.pageCount = function() {
+    		    return Math.ceil($scope.entities.length/$scope.itemsPerPage)-1;
+    		  };
+
+    		  $scope.nextPage = function() {
+    		    if ($scope.currentPage < $scope.pageCount()) {
+    		      $scope.currentPage++;
+    		    }
+    		  };
+
+    		  $scope.DisableNextPage = function() {
+    		    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+    		  };
+
+    		  $scope.setPage = function(n) {
+    		    $scope.currentPage = n;
+    		  };
+    	 
+    	 
+    	 
+    	 
+        $http({
+            method : "GET",
+            //url : "http://172.21.76.189/MicrogridApi/devices"
+            url: "http://172.21.76.125:8080/MicrogridApi/devices/26/data"
+        }).then(function mySucces(response) {
+        	console.log(response.data);
+            $scope.entities = response.data;
+            $scope.entity = response.data[0];
+
+        }, function myError(response) {
+        	console.log("error");
+        	console.log(response.data);
+            //$scope.entities = response.statusText;
+        });
+        
+        $http({
+            method : "GET",
+            //url : "http://172.21.76.189/MicrogridApi/devices"
+            url: "http://172.21.76.125:8080/MicrogridApi/devices/26"
+        }).then(function mySucces(response) {
+        	console.log(response.data);
+            $scope.device = response.data;
+            //$scope.entity = response.data[0];
+
+        }, function myError(response) {
+        	console.log("error");
+        	console.log(response.data);
+            //$scope.entities = response.statusText;
+        });
+    });
    
 
 }());
+
--- webapp/src/main/webapp/js/init.js
+++ webapp/src/main/webapp/js/init.js
@@ -22,6 +22,11 @@
                 .when('/login',
                         {templateUrl: 'partials/login.html',
                             publicAccess: true})
+                .when('/monitor',
+                    {
+                	controller: 'devicedetailCtrl',
+                	templateUrl: 'partials/devices/monitor.html',
+                            publicAccess: true})
                 .when('/devices',
                         {controller: 'DevicesController',
                             templateUrl: 'partials/devices/list.html',
@@ -48,76 +53,9 @@
 //        $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
     });
-
-
-    as.run(function ($rootScope, $http, $route, $location, base64) {
-        //make current message accessible to root scope and therefore all scopes
-        $rootScope.message = function () {
-            return message;
-        };
-
-        /**
-         * Holds all the requests which failed due to 401 response.
-         */
-        $rootScope.requests401 = [];
-
-        $rootScope.$on('event:loginRequired', function () {
-            //$('#login').modal('show');
-            $location.path('/login');
-        });
-
-        /**
-         * On 'event:loginConfirmed', resend all the 401 requests.
-         */
-        $rootScope.$on('event:loginConfirmed', function () {
-            var i,
-                    requests = $rootScope.requests401,
-                    retry = function (req) {
-                        $http(req.config).then(function (response) {
-                            req.deferred.resolve(response);
-                        });
-                    };
-
-            for (i = 0; i < requests.length; i += 1) {
-                retry(requests[i]);
-            }
-
-            $rootScope.requests401 = [];
-            $location.path('/devices');
-        });
-
-        /**
-         * On 'event:loginRequest' send credentials to the server.
-         */
-        $rootScope.$on('event:loginRequest', function (event, username, password) {
-            httpHeaders.common['Authorization'] = 'Basic ' + base64.encode(username + ':' + password);
-            console.log('httpHeaders.common[\'Authorization\']@' + httpHeaders.common['Authorization'] + ':::' + username + ':' + password);
-            $http.get('api/me')
-                    .success(function (data) {
-                        $rootScope.authenticated = true;
-                        $rootScope.name = data.username;
-                        $rootScope.$broadcast('event:loginConfirmed');
-                    })
-                    .error(function (data) {
-                        console.log('login failed...@' + data);
-                    });
-        });
-
-        /**
-         * On 'logoutRequest' invoke logout on the server and broadcast 'event:loginRequired'.
-         */
-     
-
-        //$rootScope.$on('$viewContentChange', funtion());
-        //check the networking connection.
-
-        $http.get('api/ping')
-                .success(function (data) {
-                    console.log("ping result@"+data);
-                })
-                .error(function (data) {
-                     $rootScope.message={text:'Network connection eror!', type:'danger', show:true};
-                });
-    });
+    
+    
+    
+    
 
 }());
