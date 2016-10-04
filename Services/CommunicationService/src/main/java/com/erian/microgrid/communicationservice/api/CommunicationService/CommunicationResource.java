0ba49c0010498e3cbfdf781861d4e10430bb85b1
STAFF\neelima_benny <neelima_benny@ntu.edu.sg>
2016-10-04 11:13:32 +0800

    communication service -changing api name to suit naming conventions

--- Services/CommunicationService/src/main/java/com/erian/microgrid/communicationservice/api/CommunicationService/CommunicationResource.java
+++ Services/CommunicationService/src/main/java/com/erian/microgrid/communicationservice/api/CommunicationService/CommunicationResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Produces;
 
 import org.json.simple.JSONObject;
 
-@Path("Data")
+@Path("devicedata")
 
 public class CommunicationResource {
 
@@ -18,7 +18,7 @@ public class CommunicationResource {
 @Path("/{ipAddress}/{command}")
 @Produces(MediaType.APPLICATION_JSON)
 
-public JSONObject getDataFromDevices(@PathParam("ipAddress") String ipAddress, @PathParam("command") String command) throws IOException{
+public JSONObject getDataFromDevices(@PathParam("ipAddress") String ipAddress, @PathParam("command") String command) throws IOException, InterruptedException{
 	System.out.println(ipAddress);
 	System.out.println(command);
 	String formattedCommand= command + "\n";
