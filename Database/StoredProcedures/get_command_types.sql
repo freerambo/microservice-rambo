1fe332ee39e7d4058008c832e3f66a4b1b82f335
Sasha <afridman@ntu.edu.sg>
2016-10-06 13:04:24 +0800

    DB : Fixed a bug in get_command_types SP

--- Database/StoredProcedures/get_command_types.sql
+++ Database/StoredProcedures/get_command_types.sql
@@ -2,6 +2,6 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `get_command_types`()
 BEGIN
 
 SELECT id as id, name as name, description as description 
-FROM `smes_microgrid_dev`.`command_command_types`;
+FROM `smes_microgrid`.`command_type`;
 
 END
