c8be3763095794ba5eed9af9660a7feb23097d87
Sasha <afridman@ntu.edu.sg>
2016-10-07 17:54:54 +0800

    363 : DB : Increased size of allowed text for GROUP_CONCAT (otherwise JSON would be truncated) added some new columns to the output

--- Database/StoredProcedures/get_devices_data_latest.sql
+++ Database/StoredProcedures/get_devices_data_latest.sql
@@ -1,11 +1,23 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_devices_data_latest`()
 BEGIN
 
+-- This is important as the MySQL default setting is 1024!
+SET SESSION group_concat_max_len = 500000;
+
 SELECT CONCAT('{"Devices" : [', GROUP_CONCAT(CONCAT('{"DeviceId":"', deviceId, '", "DeviceName":"', deviceName, '", "Variables":[', list, ']}')), ']}') as jsonStr 
 FROM 
 (SELECT
   deviceId, deviceName,
-  GROUP_CONCAT(CONCAT('{"VariableId":"', VariableId, '", "VariableName":"', VariableName, '", "LatestValue":"',latestValue,'", "ValueTimestamp":"', valueTimestamp,'"}')) list
+  GROUP_CONCAT(CONCAT('{"VariableId":"', VariableId, '", "VariableName":"', VariableName, '", "LatestValue":"', IFNULL(latestValue, ''),'", "ValueTimestamp":"', IFNULL(valueTimestamp, ''),
+  '", "URL_ON":"', URL_On,
+  '", "URL_OFF":"', URL_Off,
+  '", "IsSwitcher":"', CASE WHEN switchOnCommandId IS NOT NULL  
+								THEN 1 ELSE 0 
+						END,
+  '", "IsLink":"', CASE WHEN URL_On ='' AND   URL_OFF = ''
+								THEN 0 ELSE 1 
+						END,                        
+  '"}')) list
 FROM
   smes_microgrid.device_data_view
 GROUP BY
