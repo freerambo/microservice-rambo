539bcf5aa02744fde33f4049412402ebf5fb1f42
Sasha <afridman@ntu.edu.sg>
2016-10-07 18:06:49 +0800

    0: DB : LATEST MIGRATION SCRIPT

--- Database/MigrationScripts/migration_script.sql
+++ Database/MigrationScripts/migration_script.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `smes_microgrid` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `smes_microgrid`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: smes_microgrid
@@ -2179,4 +2181,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-07 17:48:48
+-- Dump completed on 2016-10-07 18:02:55
