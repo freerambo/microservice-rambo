24b1448e6513904cee310fb12b442df6270f830e
Sasha <afridman@ntu.edu.sg>
2016-10-07 17:14:27 +0800

    372, 373 : DB : SP Changes get_switch_ON and get_switch_off _command

--- Database/StoredProcedures/get_switch_OFF_command.sql
+++ Database/StoredProcedures/get_switch_OFF_command.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_switch_OFF_command`(IN device_id INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_switch_OFF_command`(IN device_id INT, IN variable_id INT)
 BEGIN
 
 -- SELECT * FROM smes_microgrid.command_type;
@@ -12,6 +12,7 @@ BEGIN
 SELECT  
 		C.format_string as commandFormatString,
         C.id as commandId,
+        C.name as commandName,
         C.command_protocol_id as protocolId,
         C.command_type_id as commandTypeId,
         D.id as deviceId,
@@ -20,11 +21,16 @@ SELECT
 		CP.id as protocolId,
 		CP.name as protocolName,
 		CT.id as commandTypeId,
-		CT.name as commandTypeName
+		CT.name as commandTypeName,
+        CDV.variable_id as statusVariableId,
+        V.name as statusVariableName
 FROM device 			as D 
 INNER JOIN command 			as C ON C.device_id = D.id
+INNER JOIN command_device_variable CDV ON CDV.command_id = C.id AND CDV.variable_id = variable_id 
+INNER JOIN variable V ON V.id = variable_id 
 INNER JOIN command_protocol as CP ON CP.id = C.command_protocol_id
 INNER JOIN command_type 	as CT ON CT.id = C.command_type_id
-WHERE D.id = device_id AND CT.id = 12;  -- Command type 11 is 'Switch On'
+WHERE D.id = device_id AND CT.id = 12 -- Command type 11 is 'Switch On'
+LIMIT 1;
 
 END
--- Database/StoredProcedures/get_switch_ON_command.sql
+++ Database/StoredProcedures/get_switch_ON_command.sql
@@ -1,5 +1,10 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_switch_ON_command`(IN device_id INT)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_switch_ON_command`(IN device_id INT, IN variable_id INT)
 BEGIN
+
+-- Parameter variable_id used to return switchON command when there are more than one CHANNELS
+-- Channel_1_Status is one variable with some ON/OFF commands,
+-- Channel_2_Status is another variable with some ON/OFF commands
+
 
 -- SELECT * FROM smes_microgrid.command_type;
 
@@ -11,6 +16,7 @@ BEGIN
 
 SELECT  C.format_string as commandFormatString,
         C.id as commandId,
+        C.name as commandName,
         C.command_protocol_id as protocolId,
         C.command_type_id as commandTypeId,
         D.id as deviceId,
@@ -19,11 +25,16 @@ SELECT  C.format_string as commandFormatString,
     CP.id as protocolId,
     CP.name as protocolName,
     CT.id as commandTypeId,
-	CT.name as commandTypeName
+	CT.name as commandTypeName,
+    CDV.variable_id as statusVariableId,
+    V.name as statusVariableName 
 FROM device 			as D 
 INNER JOIN command 			as C ON C.device_id = D.id
+INNER JOIN command_device_variable CDV ON CDV.command_id = C.id AND CDV.variable_id = variable_id 
+INNER JOIN variable V ON V.id = variable_id 
 INNER JOIN command_protocol as CP ON CP.id = C.command_protocol_id
 INNER JOIN command_type 	as CT ON CT.id = C.command_type_id
-WHERE D.id = device_id AND CT.id = 11;  -- Command type 11 is 'Switch On'
+WHERE D.id = device_id  AND CT.id = 11 -- Command type 11 is 'Switch On'
+LIMIT 1;  
 
 END
