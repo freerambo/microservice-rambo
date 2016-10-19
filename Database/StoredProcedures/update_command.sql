eea0525967b2f87d85e7e15a8f0e343db01d44cb
Sasha <afridman@ntu.edu.sg>
2016-10-19 13:07:18 +0800

    393 :DB : added inout and output parameters to commands-related SPs

--- Database/StoredProcedures/update_command.sql
+++ Database/StoredProcedures/update_command.sql
@@ -11,7 +11,7 @@ BEGIN
 DECLARE EXIT HANDLER FOR SQLEXCEPTION
 BEGIN	
 -- If some part of loading wasn't successful, continue with next steps but log the problem
-	CALL smes_microgrid.log_error('smes_microgrid.add_command');
+	CALL smes_microgrid_dev.log_error('smes_microgrid_dev.add_command');
     ROLLBACK; -- NOTE: Rollback statement should come AFTER Get Diagnostics  (that is inside log_error sp)
     RESIGNAL;
     -- SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERROR: An error occurred when ADDING NEW Command';
@@ -20,10 +20,10 @@ END;
 START TRANSACTION;	
 
 -- Log the start of excecution
-	CALL smes_microgrid.log_info('smes_microgrid.update_command', CONCAT('Start Updateing COMMAND to with CommandID =', command_id, ' Values Are: Name= ', name));
+	CALL smes_microgrid_dev.log_info('smes_microgrid_dev.update_command', CONCAT('Start Updateing COMMAND to with CommandID =', command_id, ' Values Are: Name= ', name));
 -- End of Logging
 
-	UPDATE `smes_microgrid`.`command`
+	UPDATE `smes_microgrid_dev`.`command`
 	SET
 	`name` = name,
 	`description` = description,
@@ -34,7 +34,7 @@ START TRANSACTION;
 
 	
    -- Log the end of excecution
-		CALL smes_microgrid.log_info('smes_microgrid.update_command', CONCAT('Command Updated, CommandID=', command_id, ' New Values are: Name =', name, ' , Description=', description, ' , Format String=', format_string));
+		CALL smes_microgrid_dev.log_info('smes_microgrid_dev.update_command', CONCAT('Command Updated, CommandID=', command_id, ' New Values are: Name =', name, ' , Description=', description, ' , Format String=', format_string));
 	-- End of Logging
     
     -- For parameter types 1 and 2 are hardcoded:
@@ -43,24 +43,24 @@ START TRANSACTION;
 	-- 2	Output	output of the command
 
 	-- Delete all references for this command before inserting new ones:
-	DELETE FROM `smes_microgrid`.`command_device_variable`
+	DELETE FROM `smes_microgrid_dev`.`command_device_variable`
 	WHERE command_id = command_id ;
 
 
-	INSERT INTO `smes_microgrid`.`command_device_variable`
+	INSERT INTO `smes_microgrid_dev`.`command_device_variable`
 	(`command_id`,
 	`variable_id`,
 	`parameter_type_id`)
 	SELECT command_id , variable.id, 1 FROM variable WHERE FIND_IN_SET(variable.id, input_variables);
 
- 	INSERT INTO `smes_microgrid`.`command_device_variable`
+ 	INSERT INTO `smes_microgrid_dev`.`command_device_variable`
 	(`command_id`,
 	`variable_id`,
 	`parameter_type_id`)
 	SELECT command_id , variable.id, 2 FROM variable WHERE FIND_IN_SET(variable.id, output_variables);                                       
     
   -- Log the end of excecution
-		CALL smes_microgrid.log_info('smes_microgrid.update_command', CONCAT('Added INPUT and OUTPUT variables to the Command:', command_id));
+		CALL smes_microgrid_dev.log_info('smes_microgrid_dev.update_command', CONCAT('Added INPUT and OUTPUT variables to the Command:', command_id));
 	-- End of Logging                                          
 	
 	COMMIT;  
@@ -74,11 +74,15 @@ START TRANSACTION;
     CP.id as protocolId,
     CP.name as protocolName,
     CT.id as commandTypeId,
-	CT.name as commandTypeName
-	FROM `smes_microgrid`.`command` as C
+	CT.name as commandTypeName,
+    CDV_IN.variable_id as inputVariableId,
+    CDV_out.variable_id as outputVariableId
+	FROM command as C
 	INNER JOIN command_protocol as CP ON CP.id = C.command_protocol_id
-	INNER JOIN command_type 	as CT ON CT.id = C.command_type_id    
-	WHERE C.id = command_id 
+	INNER JOIN command_type 	as CT ON CT.id = C.command_type_id
+	LEFT JOIN command_device_variable CDV_IN ON CDV_IN.command_id = C.id and CDV_IN.parameter_type_id = 1
+	LEFT JOIN command_device_variable CDV_OUT ON CDV_OUT.command_id = C.id and CDV_OUT.parameter_type_id = 2	
+    WHERE C.id = command_id 
 	;
  
 END
