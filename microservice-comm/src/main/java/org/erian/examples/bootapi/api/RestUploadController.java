02d238024c0767c72a45b5b04c823276418244bc
freerambo <asybzhu@gmail.com>
2018-01-30 21:30:22 +0800

    rm java8 libs

--- microservice-comm/src/main/java/org/erian/examples/bootapi/api/RestUploadController.java
+++ microservice-comm/src/main/java/org/erian/examples/bootapi/api/RestUploadController.java
@@ -1,13 +1,11 @@
 package org.erian.examples.bootapi.api;
 
 
-import org.erian.modules.utils.Collections3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +20,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class RestUploadController {
